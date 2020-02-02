1b2ad390f58c80c7d0b871a8994c9498c412f8ea
eclarke12 <42503461+eclarke12@users.noreply.github.com>
2020-02-02 14:14:58 -0800

    First functional version

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# gm-scene-background
+![https://img.shields.io/badge/Foundry%20VTT-0.4.4%2B-green](https://img.shields.io/badge/Foundry%20VTT-0.4.4%2B-green)
+
+# GM Scene Background
+
+Allows GMs to set a GM-only background image.
+
+## Usage
+Open the Scene Configuration form and set a GM background using the filepicker:
+
+[![Video Demo](https://img.youtube.com/vi/VID/0.jpg)](https://www.youtube.com/watch?v=uxlI7zBYHCc)
---
+++ gm-bg.js
@@ -0,0 +1,95 @@
+Hooks.on("renderSceneSheet", (app, html, data) => {
+    const gmFlag = app.entity.getFlag("gm-bg", "gm-img");
+    const pcFlag = app.entity.getFlag("gm-bg", "pc-img");
+    const bg = app.entity.img;
+    
+    const gmBGHtml = `
+    <p class="notes">This shows the background image currently displayed to you. Use the filepicker to set a new DEFAULT background for the scene, which will be shown to players on Save.</p>
+    <div class="form-group">
+	    <label>GM Background Image</label>
+        <button type="button" class="file-picker" data-type="imagevideo" data-target="flags.gm-bg.gm-img" title="Browse Files" tabindex="-1">
+            <i class="fas fa-file-import fa-fw"></i>
+        </button>
+        <input class="image" type="text" name="flags.gm-bg.gm-img" value="${gmFlag || bg}" data-dtype="String" />
+    </div>
+    <p class="notes">This shows the background image currently displayed to GMs.</p>
+    <div class="form-group">
+        <label>PC Background Image</label>
+        <input class="image" type="text" name="pc-img" placeholder="File Path" value="${pcFlag || bg}" data-dtype="String" disabled />
+        <p class="notes">This is the background image currently displayed to players. Change using the Background Image filepicker.</p>
+    </div>
+    `
+
+    const bgInput = html.find(".image");
+    const formGroup = bgInput.closest(".form-group");
+    formGroup.after(gmBGHtml);
+    const button = html.find('button[data-target="flags.gm-bg.gm-img"]')[0];
+    const pcBGInput = html.find('input[name="pc-img"]');
+
+    app._activateFilePicker(button);
+    /*
+    bgInput.on("change", event => {
+        pcBGInput.val(event.target.value);
+    });
+    */
+});
+
+Hooks.on("canvasInit", async canvas => {
+    // Non-GM users should always render the default background
+    if (!game.user.isGM) {
+        return;
+    }
+
+    const bg = canvas.scene.data.img;
+    const gmFlag = canvas.scene.getFlag("gm-bg", "gm-img");
+    const pcFlag = canvas.scene.getFlag("gm-bg", "pc-img")
+
+    // Check that there is a GM background set, and if the scene background doesn't match, swap it out
+    if ( gmFlag && bg !== gmFlag) {
+  
+        canvas.scene.data.img = gmFlag;
+        return canvas.draw();
+    }
+});
+
+Hooks.on("preUpdateScene", (scene, updateData, options) => {
+    const bgUpdate = getProperty(updateData, "img");
+    //const gmBGUpdate = getProperty(updateData, "flags.gm-bg.gm-img");
+
+    
+    if (!bgUpdate) {
+        return;
+    }
+
+    //const bg = bgUpdate ? updateData.img : scene.img;
+    const pcFlag = canvas.scene.getFlag("gm-bg", "pc-img");
+
+    // If the flag doesn't match the background
+    if (pcFlag !== bgUpdate) {
+        setProperty(updateData, "flags.gm-bg.pc-img", bgUpdate);
+    }
+});
+
+Hooks.on("updateScene", (scene, updateData, userId) => {
+    let changed = new Set(Object.keys(updateData).filter(k => k !== "_id"));
+
+    const redraw = [
+        "backgroundColor", "drawings", "gridType", "grid", "gridAlpha", "gridColor", "gridDistance", "gridUnits",
+        "shiftX", "shiftY", "width", "height", "img", "tokenVision", "globalLight", "fogExploration",
+        "lights", "sounds", "templates", "tiles", "tokens", "walls", "weather"
+    ];
+
+    // The scene is already going to redraw, so nothing to do
+    if  ( redraw.some(k => changed.has(k))) {
+        return;
+    }
+
+    const gmFlag = canvas.scene.getFlag("gm-bg", "gm-img");
+    const pcFlag = canvas.scene.getFlag("gm-bg", "pc-img"); 
+    
+    // If I am a GM user and the background does not match the flag, redraw
+    // If I am not a GM user and the background does not match the PC flag, redraw
+    if ((game.user.isGM && gmFlag && scene.img !== gmFlag) || (!game.user.isGM && pcFlag && scene.img !== pcFlag)) {  
+        return canvas.draw();
+    }
+});
